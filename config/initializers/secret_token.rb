e2aadcf2625c41fbe9b660af917870418d5ef872
unknown <Mike@.(none)>
2011-04-07 20:28:55 -0700

    Authentication Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authentication::Application.config.secret_token = '733b1949010bc7963cd0e595383631a50fa325471acba72fba8a5b0484f2dfb8a6dc2455115ec2aff2080ee9646d0f9ff9480fc5a67aabd634c954eac86e4896'
